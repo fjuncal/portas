bf9b25a1a7071e574af62c9978c67465f7552df8
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-14 17:31:04 -0300

    Criando e adicionando componente de Entrada Numerica na pagina inicial

---
+++ src/components/EntradaNumerica.tsx
@@ -0,0 +1,16 @@
+import styles from "../styles/EntradaNumerica.module.css";
+
+interface EntradaNumericaProps {
+  text?: string;
+  value?: number;
+  onChange: (novoValor: number) => void;
+}
+
+export default function EntradaNumerica(props: EntradaNumericaProps) {
+  return (
+    <div className={styles.entradaNumerica}>
+      <span className={styles.text}>{props.text}</span>
+      <span className={styles.value}>{props.value}</span>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
 import Cartao from "../components/Cartao";
 import styles from "../styles/Formulario.module.css";
+import EntradaNumerica from "../components/EntradaNumerica";
+import { useState } from "react";
 
 export default function Formulario() {
+  const [qtdePortas, setQtdePortas] = useState(3);
+  const [comPresente, setComPresente] = useState(1);
+
   return (
     <>
       <div className={styles.formulario}>
@@ -10,10 +15,24 @@ export default function Formulario() {
           <Cartao bgcolor="#c0392c">
             <h1>Monty Hall</h1>
           </Cartao>
-          <Cartao />
+          <Cartao>
+            <EntradaNumerica
+              text="Qtde Portas?"
+              value={qtdePortas}
+              onChange={(novaQtdePortas) => setQtdePortas(novaQtdePortas)}
+            />
+          </Cartao>
         </div>
         <div>
-          <Cartao />
+          <Cartao>
+            <EntradaNumerica
+              text="Porta com presente?"
+              value={comPresente}
+              onChange={(novaPortaComPresente) =>
+                setComPresente(novaPortaComPresente)
+              }
+            />
+          </Cartao>
           <Cartao bgcolor="#28a085">
             <Link href={`/jogo/4/2`} className={styles.link}>
               <h2>Iniciar</h2>
