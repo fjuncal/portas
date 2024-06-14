381f46a09c8e8eb11673857b216271e10d4205ed
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-14 00:51:54 -0300

    Criando modelo, adicionando typescript no projeto

---
+++ model/portaModel.ts
@@ -0,0 +1,64 @@
+export class PortaModel {
+  numero: number;
+  temPresente: boolean;
+  selecionada: boolean;
+  aberta: boolean;
+
+  constructor(
+    numero,
+    temPresente = false,
+    selecionada = false,
+    aberta = false
+  ) {
+    this.numero = numero;
+    this.temPresente = temPresente;
+    this.selecionada = selecionada;
+    this.aberta = aberta;
+  }
+
+  getNumero() {
+    return this.numero;
+  }
+
+  getTemPresente() {
+    return this.temPresente;
+  }
+
+  getSelecionada() {
+    return this.selecionada;
+  }
+
+  getAberta() {
+    return this.aberta;
+  }
+
+  desselecionar() {
+    const selecionada = false;
+    return new PortaModel(
+      this.numero,
+      this.temPresente,
+      selecionada,
+      this.aberta
+    );
+  }
+
+  alternarSelecao() {
+    const selecionada = !this.selecionada;
+    return new PortaModel(
+      this.numero,
+      this.temPresente,
+      selecionada,
+      this.aberta
+    );
+  }
+
+  abrir() {
+    const aberta = true;
+    return new PortaModel(
+      this.numero,
+      this.temPresente,
+      this.selecionada,
+      aberta
+    );
+  }
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,13 @@
-import Porta from "@/components/Porta";
-import Presente from "@/components/Presente";
-
+import { useState } from "react";
+import { PortaModel } from "../../model/portaModel";
+import Porta from "../components/Porta";
 export default function Home() {
+  const [p1, setp1] = useState(new PortaModel(1));
+
   return (
     <>
       <div>
-        <Porta selecionada={true} />
-        <Porta selecionada={false} />
+        <Porta porta={p1} />
       </div>
     </>
   );
