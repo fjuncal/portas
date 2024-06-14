8db078732ac25e9c209f5528fa060e3c40f2610c
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-14 01:06:25 -0300

    Adicionando click na porta e adicionando metodo na interface de onChange

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { PortaModel } from "../../model/portaModel";
 import Porta from "../components/Porta";
+import { log } from "console";
 export default function Home() {
-  const [p1, setp1] = useState(new PortaModel(1));
+  const [p1, setP1] = useState(new PortaModel(1));
+  const [texto, setTexto] = useState("...");
 
   return (
     <>
       <div>
-        <Porta porta={p1} />
+        <Porta value={p1} onChange={(novaPorta) => setP1(novaPorta)} />
       </div>
     </>
   );
